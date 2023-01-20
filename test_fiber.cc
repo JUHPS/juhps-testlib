2802bc60ad479e4a0cef0fe81b17262e15bbc020
jujimeizuo <fengzetao.zed@foxmail.com>
2023-01-20 20:56:39 +0800

    :art:feat(fiber): test_fiber done

--- test_fiber.cc
+++ test_fiber.cc
@@ -1,5 +1,6 @@
 #include "../server/src/jujimeizuo.h"
 
+
 jujimeizuo::Logger::ptr g_logger = JUJIMEIZUO_LOG_ROOT();
 
 void run_in_fiber() {
@@ -8,12 +9,28 @@ void run_in_fiber() {
     JUJIMEIZUO_LOG_INFO(g_logger) << "run_in_fiber end";
 }
 
+void test_fiber() {
+    JUJIMEIZUO_LOG_INFO(g_logger) << "main begin -1";
+    {
+        jujimeizuo::Fiber::GetThis();
+        JUJIMEIZUO_LOG_INFO(g_logger) << "main begin";
+        jujimeizuo::Fiber::ptr fiber(new jujimeizuo::Fiber(run_in_fiber));
+        fiber -> swapIn();
+        JUJIMEIZUO_LOG_INFO(g_logger) << "main after swapIn";
+        fiber -> swapIn();
+        JUJIMEIZUO_LOG_INFO(g_logger) << "main after end";
+    }
+    JUJIMEIZUO_LOG_INFO(g_logger) << "main after end2";
+}
+
 int main(int argc, char** argv) {
-    jujimeizuo::Fiber::GetThis();
-    JUJIMEIZUO_LOG_INFO(g_logger) << "main begin";
-    jujimeizuo::Fiber::ptr fiber(new jujimeizuo::Fiber(run_in_fiber));
-    fiber -> swapIn();
-    JUJIMEIZUO_LOG_INFO(g_logger) << "main after swapIn";
-    fiber -> swapIn();
-    JUJIMEIZUO_LOG_INFO(g_logger) << "main after end";
+    jujimeizuo::Thread::SetName("main");
+    std::vector<jujimeizuo::Thread::ptr> thrs;
+    for (int i = 0; i < 3; i++) {
+        thrs.push_back(jujimeizuo::Thread::ptr(new jujimeizuo::Thread(&test_fiber, "name_" + std::to_string(i))));
+    }
+    for (auto i : thrs) {
+        i -> join();
+    }
+    return 0;
 }
