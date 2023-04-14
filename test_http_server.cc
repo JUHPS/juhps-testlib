af5a5cde945c2d2d5f1a9df908b393005bb70826
jujimeizuo <3058852685@qq.com>
2023-04-14 16:01:00 +0000

    [feat]: update testlib

--- test_http_server.cc
+++ test_http_server.cc
@@ -1,9 +1,3 @@
-/**
- * @file test_http_server.cc
- * @brief HttpServer测试
- * @version 0.1
- * @date 2021-09-28
- */
 #include "src/jujimeizuo.h"
 
 static jujimeizuo::Logger::ptr g_logger = JUJIMEIZUO_LOG_ROOT();
@@ -12,31 +6,28 @@ static jujimeizuo::Logger::ptr g_logger = JUJIMEIZUO_LOG_ROOT();
 
 jujimeizuo::IOManager::ptr worker;
 
+
 void run() {
     g_logger->setLevel(jujimeizuo::LogLevel::INFO);
-    //jujimeizuo::http::HttpServer::ptr server(new jujimeizuo::http::HttpServer(true, worker.get(), jujimeizuo::IOManager::GetThis()));
     jujimeizuo::http::HttpServer::ptr server(new jujimeizuo::http::HttpServer(true));
     jujimeizuo::Address::ptr addr = jujimeizuo::Address::LookupAnyIPAddress("0.0.0.0:8020");
     while (!server->bind(addr)) {
         sleep(2);
     }
     auto sd = server->getServletDispatch();
-    sd->addServlet("/jujimeizuo/xx", [](jujimeizuo::http::HttpRequest::ptr req, jujimeizuo::http::HttpResponse::ptr rsp, jujimeizuo::http::HttpSession::ptr session) {
-        rsp->setBody(req->toString());
-        return 0;
+    
+    sd->addServlet("/jujimeizuo/hello", [](jujimeizuo::http::HttpRequest::ptr req, jujimeizuo::http::HttpResponse::ptr rsp, jujimeizuo::http::HttpSession::ptr session){
+      rsp->setBody(rsp->toString());
+      return 0;
     });
+
 
     sd->addGlobServlet("/jujimeizuo/*", [](jujimeizuo::http::HttpRequest::ptr req, jujimeizuo::http::HttpResponse::ptr rsp, jujimeizuo::http::HttpSession::ptr session) {
-        rsp->setBody("Glob:\r\n" + req->toString());
-        return 0;
-    });
-
-    sd->addGlobServlet("/jujimeizuox/*", [](jujimeizuo::http::HttpRequest::ptr req, jujimeizuo::http::HttpResponse::ptr rsp, jujimeizuo::http::HttpSession::ptr session) {
         rsp->setBody(XX(<html>
                                 <head><title> 404 Not Found</ title></ head>
                                 <body>
                                 <center><h1> 404 Not Found</ h1></ center>
-                                <hr><center>
+                                   <hr><center>
                                     nginx /
                                 1.16.0 <
                             / center >
@@ -62,7 +53,7 @@ void run() {
 int main(int argc, char **argv) {
     jujimeizuo::EnvMgr::GetInstance()->init(argc, argv);
     jujimeizuo::Config::LoadFromConfDir(jujimeizuo::EnvMgr::GetInstance()->getConfigPath());
-    
+    jujimeizuo::Config::LoadFromStaticDir(jujimeizuo::EnvMgr::GetInstance()->getStaticPath());
     jujimeizuo::IOManager iom(1, true, "main");
     worker.reset(new jujimeizuo::IOManager(3, false, "worker"));
     iom.schedule(run);
